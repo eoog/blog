3da5dc27d8e53a032ca103af18bd27267f856b65
en <qkqhemforhs1@gmail.com>
2024-08-27 09:39:35 +0900

    spring-scheudler

---
+++ spring-scheudler/src/main/java/com/www/springscheudler/SpringScheudlerApplication.java
@@ -0,0 +1,13 @@
+package com.www.springscheudler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScheudlerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringScheudlerApplication.class, args);
+  }
+
+}
---
+++ spring-scheudler/src/test/java/com/www/springscheudler/SpringScheudlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.www.springscheudler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScheudlerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
