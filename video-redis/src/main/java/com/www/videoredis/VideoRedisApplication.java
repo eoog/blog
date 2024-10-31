286627fd6fb04297e60bd3ba5ef7ed0f5c2e01ce
en <kimminwoo0106@gmail.com>
2024-10-31 11:02:53 +0900

    init

--- video-redis/src/main/java/com/www/videoredis/VideoRedisApplication.java
+++ video-redis/src/main/java/com/www/videoredis/VideoRedisApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VideoRedisApplication {
 
+
   public static void main(String[] args) {
     SpringApplication.run(VideoRedisApplication.class, args);
   }
