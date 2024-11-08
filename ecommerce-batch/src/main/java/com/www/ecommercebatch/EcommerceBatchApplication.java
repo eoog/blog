b704455ca2f84a9d90a9a109ef1ce47743485e78
mw <kimminwoo0106@gmail.com>
2024-11-08 21:48:54 +0900

    11

---
+++ ecommerce-batch/src/main/java/com/www/ecommercebatch/EcommerceBatchApplication.java
@@ -0,0 +1,13 @@
+package com.www.ecommercebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBatchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EcommerceBatchApplication.class, args);
+  }
+
+}
---
+++ ecommerce-batch/src/test/java/com/www/ecommercebatch/EcommerceBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.www.ecommercebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
