78e3e87c05783f03e4f0e8ba39f0626ec986029a
user <kimminwoo0106@gmail.com>
2025-01-06 11:06:19 +0900

    1.spring-batch-log 기반 kafka send 적용완료

---
+++ example/spring-log-batch/src/main/java/com/www/springlogbatch/SpringLogBatchApplication.java
@@ -0,0 +1,13 @@
+package com.www.springlogbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLogBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLogBatchApplication.class, args);
+	}
+
+}
