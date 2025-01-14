b7bcabde235209459c75834d563d4f51b461a644
user <kimminwoo0106@gmail.com>
2025-01-14 11:17:17 +0900

    1. kafka-producer , consumer 2. 각각 @Queryfilter 로 값 다르게

--- example/kafka/src/main/java/com/www/kafka/consumer/MyConsumer.java
+++ example/kafka/src/main/java/com/www/kafka/consumer/MyConsumer.java
@@ -1,15 +1,21 @@
 package com.www.kafka.consumer;
 
 import com.www.kafka.model.MyMessage;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Consumer;
 
 @Component
-public class MyConsumer implements Consumer<Message<MyMessage>> {
-  @Override
-  public void accept(Message<MyMessage> message) {
-    System.out.println("Message arrived! - " + message.getPayload());
+public class MyConsumer  {
+
+  @KafkaListener(
+      topics = {"my-json-topic"},
+      groupId = "test-consumer-group"
+  )
+  public void accept(ConsumerRecord<String, MyMessage> message) {
+    System.out.println("[Main Consumer] Message arrived! - " + message.value());
   }
 }
