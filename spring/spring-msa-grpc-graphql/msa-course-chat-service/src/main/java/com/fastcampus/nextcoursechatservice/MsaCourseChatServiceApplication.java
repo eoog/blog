0cfaffb60209f9c9a6e51ff4c14dd9f9da4fcf69
user <kimminwoo0106@gmail.com>
2025-01-15 16:31:49 +0900

    1. web

---
+++ spring/spring-msa-grpc-graphql/msa-course-chat-service/src/main/java/com/fastcampus/nextcoursechatservice/MsaCourseChatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.nextcoursechatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaCourseChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaCourseChatServiceApplication.class, args);
+	}
+
+}
