cea57722a3719f0cf65f46d92c8ce953dbe7732d
user <kimminwoo0106@gmail.com>
2025-01-15 16:15:40 +0900

    1. web

--- spring/spring-msa-grpc-graphql/msa-campus-web/src/apollo.js
+++ spring/spring-msa-grpc-graphql/msa-campus-web/src/apollo.js
@@ -1,48 +1,55 @@
 // src/apollo.js
-import {ApolloClient, InMemoryCache, createHttpLink, split} from '@apollo/client/core';
-import { setContext } from '@apollo/client/link/context';
-import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  split
+}                          from '@apollo/client/core';
+import {setContext}        from '@apollo/client/link/context';
+import {GraphQLWsLink}     from "@apollo/client/link/subscriptions";
 import {getMainDefinition} from "@apollo/client/utilities";
-import { createClient } from "graphql-ws";
+import {createClient}      from "graphql-ws";
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:9000/graphql',
+  uri: 'http://localhost:9000/graphql',
 });
 
 const wsLink = new GraphQLWsLink(
     createClient({
-        url: "ws://localhost:9000/graphql",
-        options: {
-            reconnect: true,
-        },
+      url: "ws://localhost:9000/graphql",
+      options: {
+        reconnect: true,
+      },
     })
 );
 
-const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem('jwt');
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : '',
-        },
-    };
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem('jwt');
+  const userId = localStorage.getItem('userId');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+      'X-USER-Id': userId || ''
+    },
+  };
 });
 
 const link = split(
-    ({ query }) => {
-        const definition = getMainDefinition(query);
-        return (
-            definition.kind === 'OperationDefinition' &&
-            definition.operation === 'subscription'
-        );
+    ({query}) => {
+      const definition = getMainDefinition(query);
+      return (
+          definition.kind === 'OperationDefinition' &&
+          definition.operation === 'subscription'
+      );
     },
     wsLink,
     authLink.concat(httpLink),
 );
 
 const apolloClient = new ApolloClient({
-    link: link,
-    cache: new InMemoryCache(),
+  link: link,
+  cache: new InMemoryCache(),
 });
 
 export default apolloClient;
