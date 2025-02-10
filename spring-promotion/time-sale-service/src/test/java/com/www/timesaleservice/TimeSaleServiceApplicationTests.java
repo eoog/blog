206477246c87e829225694f618528908611c699f
user <kimminwoo0106@gmail.com>
2025-02-10 09:19:57 +0900

    타임세일

---
+++ spring-promotion/time-sale-service/src/test/java/com/www/timesaleservice/TimeSaleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.www.timesaleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSaleServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
